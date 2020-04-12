eac1389f2844b9d796e8cb4ad17e6502498b856f
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-04-12 15:37:49 -0300

    Replaces List with Sets since order of insertion wasn't necessary.

--- src/main/java/sisbase/mobbgone/Config.java
+++ src/main/java/sisbase/mobbgone/Config.java
@@ -1,19 +1,20 @@
 package sisbase.mobbgone;
 import org.bukkit.Material;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Config {
-    private List<Material> Blocks = new ArrayList<>();
+    private Set<Material> Blocks = new HashSet<>();
     public void initialize()
     {
         Set<String> BlockRegistry = Utils.getNames(Material.class);
         List<String> fromConfig = MobBGone.getPlugin(MobBGone.class)
                 .getConfig().getStringList("spawnproof-blocks");
         for (String entry:fromConfig) {
-            List<String> strings = checkName(entry, BlockRegistry);
+            Set<String> strings = checkName(entry, BlockRegistry);
             for(String response : strings)
             {
                 System.out.println("REGISTERED : " + response);
@@ -21,17 +22,16 @@ public class Config {
             }
         }
     }
-    public List<Material> getBlocks() {
-
+    public Set<Material> getBlocks() {
         return Blocks;
     }
-    private List<String> checkName(String entry, Set<String> registry)
+    private Set<String> checkName(String entry, Set<String> registry)
     {
         final String newEntry = entry.replace("*","").replace("\\*","");
-        if(entry.contains("*")) return registry.stream().filter((d) -> d.contains(newEntry.toUpperCase())).collect(Collectors.toList());
+        if(entry.contains("*")) return registry.stream().filter((d) -> d.contains(newEntry.toUpperCase())).collect(Collectors.toSet());
         else
         {
-            return  registry.stream().filter((d) -> d.equals(entry.toUpperCase())).collect(Collectors.toList());
+            return  registry.stream().filter((d) -> d.equals(entry.toUpperCase())).collect(Collectors.toSet());
         }
     }
 
